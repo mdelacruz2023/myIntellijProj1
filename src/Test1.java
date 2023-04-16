344aa14d4bf2cb0984956df9d45fd5acbaa959c6
Maria dela Cruz <ma_leonora_dela_cruz@yahoo.com>
2023-04-16 11:29:52 -0400

    Add test2 from branch

--- src/Test1.java
+++ src/Test1.java
@@ -1,4 +1,10 @@
 public class Test1 {
 
     private final String ENVIRONMENT = "DEV";
+
+    public String greet(String name) {
+        return "Hello " + name;
+    }
+
+    //This is a test 123
 }
